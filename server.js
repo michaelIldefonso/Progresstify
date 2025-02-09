f9c03b064b0b6edf4aaf251be8ffe547f1a0c5c4
Michael Ildefonso <michaelildefonso8@gmail.com>
2025-02-09 09:41:52 +0800

    0

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-
+// index.js
 const express = require('express');
-const { connectDB } = require('./config/db'); // Import DB connection setup
-const userRoutes = require('./routes/userRoutes'); // Import user routes
+const path = require('path');
+const { connectDB } = require('./src/config/db'); // Import DB connection setup
+const userRoutes = require('./src/routes/userRoutes'); // Import user routes
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,12 @@ connectDB();
 
 // Set up routes
 app.use('/api', userRoutes); // Mount user-related routes under /api
+
+app.use(express.static(path.join(__dirname, './src/public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './src/public', 'index.html'));
+});
 
 // Start the server
 app.listen(port, () => {
